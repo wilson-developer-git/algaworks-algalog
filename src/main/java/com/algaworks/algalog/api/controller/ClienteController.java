69555644936d1074cf5efe1999b136de410912c5
wilson-developer-git <wilsonprogram@gmail.com>
2022-07-21 17:06:15 -0300

    Adicionando Jpa e criando um endpoint

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,32 +1,36 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	private List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Maria Joana da Silva");
-		cliente1.setEmail("maria@gmail.com");
-		cliente1.setTelefone("71925338844");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Carlos Jose dos Santos");
-		cliente2.setEmail("carlos@gmail.com");
-		cliente2.setTelefone("71988226633");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{nome}")
+	private List<Cliente> findByNome(@PathVariable String nome) {
+		return clienteRepository.findByNome(nome);
+	
+	}
+	
+	@GetMapping("/clientes/containing/{nome}")
+	private List<Cliente> findByNomeContaining(@PathVariable String nome) {
+		return clienteRepository.findByNomeContaining(nome);
+	
 	}
 	
 }
